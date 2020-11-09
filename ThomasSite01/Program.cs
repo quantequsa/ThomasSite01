afc30fbbc80f2fcab0c7b574a349d8fd858a5e52
J K <quanteq@hotmail.com>
2020-11-09 14:39:46 -0600

    V02 - 2nd Commit  Program.cs, A.cs, A.config, csproj &.sln Site03

---
+++ ThomasSite01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThomasSite01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ThomasSite01 - Shop Info Tool!");
+        }
+    }
+}
