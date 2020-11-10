fdffcbdc46306b5d37664d861798ebad4e3f06bd
J K <quanteq@hotmail.com>
2020-11-09 18:21:04 -0600

    V03 - 3rd Commit CoffeyShop, CoffeeShopDataProvider csproj &.sln Site07

---
+++ DataAccessSite07/CoffeeShopDataProvider.cs
@@ -0,0 +1,27 @@
+﻿using DataAccessSite07.Model;
+using System.Collections.Generic;
+
+namespace DataAccessSite07
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop
+            {
+                Location = "Frankfurt",
+                BeansInStockInKg = 842,
+            };
+            yield return new CoffeeShop
+            {
+                Location = "Freiburg",
+                BeansInStockInKg = 84,
+            };
+            yield return new CoffeeShop
+            {
+                Location = "Munich",
+                BeansInStockInKg = 32,
+            };
+        }
+    }
+}
---
+++ DataAccessSite07/Model/CoffeeShop.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccessSite07.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+
+        public int BeansInStockInKg { get; set; }
+    }
+}
